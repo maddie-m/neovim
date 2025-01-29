5a0f60d3bec0daf59ba14f30a444a9745e331bf6
maddie-m <55149813+maddie-m@users.noreply.github.com>
2025-01-29 14:59:18 -0500

    Remove old

--- lua/plugins/vimtex.lua
+++
@@ -1,7 +0,0 @@
-return {
-    -- "lervag/vimtex",
-    -- config = function()
-    --     vim.g.vimtex_quickfix_open_on_warning = 0
-    --     -- vim.g.vimtex_view_method = "okular &"
-    -- end,
-}
